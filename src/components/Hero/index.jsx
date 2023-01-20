f7024550db458ee8e3bc9d6bc7e3d4de0deef9fd
Fuhad Hasan <fuhadhasan479@gmail.com>
2023-01-20 16:29:04 +0600

    completed hero component

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,37 @@
+import { PlayIcon } from "@radix-ui/react-icons";
+
+import { ReactComponent as HeroIllustration } from "../../assets/social-media.svg";
+
+import "./index.scss";
+
+import Button from "../Button";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="container">
+        <div className="hero-content">
+          <div className="hero-left">
+            <HeroIllustration width="100%" height="100%" />
+          </div>
+          <div className="hero-right">
+            <h1 className="hero-title">Work at the speed of thought</h1>
+            <p className="hero-desc">
+              Tools, tutorials, design and innovation experts, all in one place!
+              The most intuitive way to imagine your next user experience.
+            </p>
+            <div className="hero-buttons">
+              <Button size="large" variant="filled">
+                Get started
+              </Button>
+              <button type="button">
+                <PlayIcon width={20} height={20} color="#02897A" />
+                <span>Watch the video</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
